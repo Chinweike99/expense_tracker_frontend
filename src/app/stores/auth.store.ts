e7ca4dbe8a47598d768b59de4991c5d7d6ac8544
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-02 10:09:37 +0100

    auth store

--- src/app/stores/auth.store.ts
+++ src/app/stores/auth.store.ts
@@ -1,4 +1,7 @@
+import {create} from 'zustand'
+import {persist} from 'zustand/middleware'
 import { RegisterData, User } from "@/@types/types";
+import api from '../lib/api';
 
 
 
@@ -14,3 +17,62 @@ interface AuthState {
     requestPasswordReset: (email: string) => Promise<void>;
     resetPassword: (token: string, password: string) => Promise<void>
 }
+
+
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            user: null,
+            token: null,
+            isLoading: false,
+            login: async (email, password) => {
+                set({isLoading: true});
+                try{
+                    const {data} = await api.post('/auth/login', {email, password});
+                    set({user: data.user, token: data.token})
+                }finally{
+                    set({isLoading: false})
+                }
+            },
+            register: async(data) => {
+                set({isLoading: true});
+                try{
+                    await api.post('/auth/signup', data);
+                }finally{
+                    set({isLoading: false})
+                }
+            }, 
+            logout: () => {
+                api.post('/auth/logout');
+                set({user: null, token: null})
+            },
+            verifyEmail: async(token) => {
+                set({isLoading: true})
+                try{
+                    await api.get(`/auth/verify-email?token=${token}`)
+                }finally{
+                    set({ isLoading: false})
+                }
+            },
+            requestPasswordReset: async(email) => {
+                set ({ isLoading: true});
+                try {
+                    await api.post("/auth/forgot-password", { email })
+                } finally {
+                    set({ isLoading: false })
+                }
+            },
+            resetPassword: async(token, password) => {
+                set({isLoading: true});
+                try{
+                    await api.post("/auth/reset-password", {token, password});
+                } finally{
+                    set({isLoading: false});
+                }
+            }
+        }),
+        {
+            name: "auth-storage",
+        }
+    )
+)
