a514577d12cdb3bfd170268b3406d8a49d053ad3
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-03 11:54:23 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
