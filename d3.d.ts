e0608ccfd8fe17b9f22f100a4f509e0aff00c3cc
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-10 19:07:54 +0100

    install packages

---
+++ d3.d.ts
@@ -0,0 +1,16 @@
+declare module 'd3' {
+    export * from 'd3-selection';
+    export * from 'd3-scale';
+    export * from 'd3-array';
+    export * from 'd3-axis';
+    export * from 'd3-shape';
+    export * from 'd3-time';
+    export * from 'd3-time-format';
+    export * from 'd3-interpolate';
+    export * from 'd3-color';
+    export const group: any;
+    export const max: any;
+    export const scaleSequential: any;
+    export const interpolateOranges: any;
+    export const select: any;
+  }
